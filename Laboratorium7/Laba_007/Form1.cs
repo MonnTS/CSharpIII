73d46fdaa490fb671e6d86b38a0202d41d089294
Daniel <73000732+Luf3r@users.noreply.github.com>
2020-12-07 20:53:59 +0300

    Add files via upload

---
+++ Laboratorium7/Laba_007/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_007
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Samochod s1 = new Samochod();
+            s1.marka = "Fiat";
+            s1.model = "98";
+            s1.paliwo = "benzyna";
+            tabSamochodow.Add(s1);
+            reflist();
+            Samochod s2 = new Samochod();
+            s2.marka = "Audi";
+            s2.model = "TT";
+            s2.paliwo = "98";
+            tabSamochodow.Add(s2);
+            reflist();
+            Samochod s3 = new Samochod();
+            s3.marka = "Pagani";
+            s3.model = "Zonda";
+            s3.paliwo = "bio3";
+            tabSamochodow.Add(s3);
+            reflist();
+        }
+
+        public void reflist()
+        {
+            listBox1.Items.Clear();
+            foreach (Samochod s in tabSamochodow)
+            {
+                listBox1.Items.Add(s.marka + " " + s.model + " " + s.paliwo);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Int32.Parse(numericUpDown1.Text);
+            int b = Int32.Parse(numericUpDown2.Text);
+            textBox4.Text = tabSamochodow[a][b];
+            reflist();
+
+        }
+
+        List<Samochod> tabSamochodow = new List<Samochod>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Samochod s = new Samochod();
+            s.marka = textBox1.Text;
+            s.model = textBox2.Text;
+            s.paliwo = textBox3.Text;
+            tabSamochodow.Add(s);
+            reflist();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Int32.Parse(textBox5.Text);
+            int b = Int32.Parse(textBox6.Text);
+            tabSamochodow[a][b] = textBox7.Text;
+            reflist();
+        }
+    }
+}
---
+++ Laboratorium7/Laba_007/Samochod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_007
+{
+    class Samochod
+    {
+        public string marka;
+        public string model;
+        public string paliwo;
+
+        public string this[int j]
+        {
+            get
+            {
+                if (j == 0) return marka;
+                else if (j == 1) return model;
+                else if (j == 2) return paliwo;
+                return "Nie podany lub zle podany";
+            }
+
+            set
+            {
+                if (j == 0) marka = value;
+                else if (j == 1) model = value;
+                else if (j == 2) paliwo = value;
+            
+            }
+        }
+
+    }
+}
