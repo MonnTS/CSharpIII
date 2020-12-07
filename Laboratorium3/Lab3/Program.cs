8ae378008f6186902bf75bfcb83be9badea1dd15
Daniel <73000732+Luf3r@users.noreply.github.com>
2020-12-07 20:51:49 +0300

    Add files via upload

---
+++ Laboratorium3/Lab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochod car1 = new Samochod("Fiat", 100);
+            Samochod car2 = new Samochod("Opel", 180);
+
+            Console.WriteLine("Samochod 1: " + car1.GetName() + ", predkosc " + car1.GetSpeed());
+            Console.WriteLine("Samochod 2: " + car2.GetName() + ", predkosc " + car2.GetSpeed());
+            Console.WriteLine("Nowy samochod +: " + (car1 + car2).GetName() + ", predkosc " + (car1 + car2).GetSpeed());
+            Console.WriteLine("Nowy samochod -: " + (car1 - car2).GetName() + ", predkosc " + (car1 - car2).GetSpeed());
+            Console.WriteLine("Nowy samochod *: " + (car1 * car2).GetName() + ", predkosc " + (car1 * car2).GetSpeed());
+            Console.WriteLine("Nowy samochod /: " + (car1 / car2).GetName() + ", predkosc " + (car1 / car2).GetSpeed());
+        }
+    }
+}
---
+++ Laboratorium3/Lab3/Samochod.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Samochod
+    {
+        private string name;
+        private double speed;
+        private static Random random = new Random();
+
+        public Samochod(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            this.speed = speed;
+        }
+
+        public double GetSpeed()
+        {
+            return speed;
+        }
+
+        public static Samochod operator +(Samochod car1, Samochod car2)
+        {
+            Samochod result = new Samochod();
+            result.name = car1.name + car2.name;
+            result.name = result.name.ToUpper();
+            result.speed = car1.speed + car2.speed;
+            return result;
+        }
+
+        public static Samochod operator -(Samochod car1, Samochod car2)
+        {
+            Samochod result = new Samochod();
+            result.name = car1.name + car2.name;
+            result.name = result.name.ToLower();
+            result.speed = car1.speed - car2.speed;
+            return result;
+        }
+
+        public static Samochod operator *(Samochod car1, Samochod car2)
+        {
+            Samochod result = new Samochod();
+            for (int i = 0; i < 10; i++)
+            {
+                result.name += Char.ConvertFromUtf32(random.Next(26) + 65);
+            }
+            result.speed = car1.speed * car2.speed;
+            return result;
+        }
+
+        public static Samochod operator /(Samochod car1, Samochod car2)
+        {
+            Samochod result = new Samochod();
+            for (int i = 0; i < 10; i++)
+            {
+                result.name += Char.ConvertFromUtf32(random.Next(26) + 65);
+            }
+            result.speed = car1.speed / car2.speed;
+            return result;
+        }
+
+
+    }
+}
+    
+
